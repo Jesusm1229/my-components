7115ca4f4222a841cf12584366991b2833b9cac7
Jesus <48733708+Jesusm1229@users.noreply.github.com>
2022-01-04 16:55:24 -0400

    Adding drag-and-drop functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { UiControlsModule } from './../../projects/ui-controls/src/lib/ui-controls.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    UiControlsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
